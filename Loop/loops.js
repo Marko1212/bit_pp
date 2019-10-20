2f810399acbeb112e637225f83bb24c511f6d17e
Your Name <you@example.com>
2019-10-20 16:14:17 +0200

    done loops exercises

--- Loop/loops.js
+++ Loop/loops.js
@@ -15,7 +15,7 @@ while (pass <= 100) {
 
 console.log(sum);
 
-
+// Write a program that will iterate from 0 to 10 and display squares of numbers.
 
 for (var x = 0; x <= 10; x++) {
     console.log(x * x);
