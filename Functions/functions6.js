e53e895aa17fa41a91a6fbdae6f1f5c2633a6a46
Your Name <you@example.com>
2019-10-30 20:37:42 +0100

    small change

--- Functions/functions6.js
+++ Functions/functions6.js
@@ -183,3 +183,55 @@ function maxElement(arr) {
 
 
 console.log(maxElement([1, undefined, "Ivana", 89, 41, -3, 1.256, NaN, Infinity, "", null]));
+
+
+
+Write a function to find the maximum and minimum elements. Function returns an array.
+
+ 
+
+Write a function to find the median element of array.
+
+ 
+
+Write a function to find the element that occurs most frequently.
+
+ 
+
+Write a function to find and return the first, middle and last element of an array if the array has odd number of elements. If number of elements is even, return just the first and the last. In other cases (empty array), input array should be returned.
+
+ 
+
+Write a function to find the average of N elements. Make the function flexible to receive dynamic number or parameters.
+
+ 
+
+Write a function to find all the numbers greater than the average.
+
+ 
+
+The body mass index (BMI) is the ratio of the weight of a person (in kilograms) to the square of the height (in meters). Write a function that takes two parameters, weight and height, computes the BMI, and prints the corresponding BMI category:
+Starvation: less than 15
+Anorexic: less than 17.5
+Underweight: less than 18.5
+Ideal: greater than or equal to 18.5 but less than 25
+Overweight: greater than or equal to 25 but less than 30
+Obese: greater than or equal to 30 but less than 40
+Morbidly obese: greater than or equal to 40
+
+ 
+
+
+Write a function that takes a list of strings and prints them, one per line, in a rectangular frame.:
+
+For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
+*********
+* Hello *
+* World *
+* in    *
+* a     *
+* frame *
+*********
+
+ 
+
